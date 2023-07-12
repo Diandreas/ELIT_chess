6288552c0f8e53ca227b47f9b9e802017e80c75c
David <davidnjandjeu@gmail.com>
2023-07-12 02:27:20 +0100

    echiquier

---
+++ chess_game/gui/diplay.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, Button
+
+root = Tk()
+root.title("Échiquier")
+root.geometry("700x700")
+white = 1;
+for i in range(8):
+    if white==1:
+        white=0
+    else:
+        white=1
+    for j in range(8):
+        if white == 1:
+            button = Button(root, text="", bg="brown" , font=("TkDefaultFont", 8), width=10, height=5)
+            button.grid(row=i, column=j)
+            white=0
+        else:
+            button = Button(root, text="", bg="white" , font=("TkDefaultFont", 8), width=10, height=5)
+            button.grid(row=i, column=j)
+            white=1
+
+
+root.mainloop()

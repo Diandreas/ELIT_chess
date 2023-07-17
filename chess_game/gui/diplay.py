0a6d128c6b06dfce9ec8c1752d0734774639b2c9
chaton2325 <138687681+chaton2325@users.noreply.github.com>
2023-07-17 18:43:24 +0100

    suppresion des images dans gui
    pour maintenir la cohérence du code

--- chess_game/gui/diplay.py
+++ chess_game/gui/diplay.py
@@ -21,55 +21,55 @@ for i in range(8):
             button.grid(row=i, column=j)
             white=1
         if i==6 and j==0:
-            image=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo=ImageTk.PhotoImage(image)
             button["image"]=photo
             button["width"]=65
             button["height"]=75
         if i==6 and j==1:
-            image1=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image1=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo1=ImageTk.PhotoImage(image1)
             button["image"]=photo1
             button["width"]=65
             button["height"]=75
         if i==6 and j==2:
-            image2=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image2=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo2=ImageTk.PhotoImage(image2)
             button["image"]=photo2
             button["width"]=65
             button["height"]=75
         if i==6 and j==2:
-            image2=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image2=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo2=ImageTk.PhotoImage(image2)
             button["image"]=photo2
             button["width"]=65
             button["height"]=75
         if i==6 and j==3:
-            image3=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image3=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo3=ImageTk.PhotoImage(image3)
             button["image"]=photo3
             button["width"]=65
             button["height"]=75
         if i==6 and j==4:
-            image4=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image4=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo4=ImageTk.PhotoImage(image4)
             button["image"]=photo4
             button["width"]=65
             button["height"]=75
         if i==6 and j==5:
-            image5=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image5=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo5=ImageTk.PhotoImage(image5)
             button["image"]=photo5
             button["width"]=65
             button["height"]=75
         if i==6 and j==6:
-            image6=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image6=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo6=ImageTk.PhotoImage(image6)
             button["image"]=photo6
             button["width"]=65
             button["height"]=75
         if i==6 and j==7:
-            image7=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            image7=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/assets/model/piece_model_1/white/pawn.png")
             photo7=ImageTk.PhotoImage(image7)
             button["image"]=photo7
             button["width"]=65

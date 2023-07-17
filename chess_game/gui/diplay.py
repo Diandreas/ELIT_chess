94b897d0e8dfb7da96c6e14c2333fa687ee71688
chaton2325 <138687681+chaton2325@users.noreply.github.com>
2023-07-17 18:28:11 +0100

    placement de pions et modifications couleurs de l'échéquier
    j'ai changer la couleur blanche de l'échéquier pour du jaune pour ne pas les confondre avec les pions blancs et j'ai placer les premiers pions simples

--- chess_game/gui/diplay.py
+++ chess_game/gui/diplay.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Button
+from PIL import Image, ImageTk
 
 root = Tk()
 root.title("Échiquier")
@@ -14,10 +15,66 @@ for i in range(8):
             button = Button(root, text="", bg="brown" , font=("TkDefaultFont", 8), width=10, height=5)
             button.grid(row=i, column=j)
             white=0
+            
         else:
-            button = Button(root, text="", bg="white" , font=("TkDefaultFont", 8), width=10, height=5)
+            button = Button(root, text="", bg="yellow" , font=("TkDefaultFont", 8), width=10, height=5)
             button.grid(row=i, column=j)
             white=1
+        if i==6 and j==0:
+            image=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo=ImageTk.PhotoImage(image)
+            button["image"]=photo
+            button["width"]=65
+            button["height"]=75
+        if i==6 and j==1:
+            image1=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo1=ImageTk.PhotoImage(image1)
+            button["image"]=photo1
+            button["width"]=65
+            button["height"]=75
+        if i==6 and j==2:
+            image2=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo2=ImageTk.PhotoImage(image2)
+            button["image"]=photo2
+            button["width"]=65
+            button["height"]=75
+        if i==6 and j==2:
+            image2=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo2=ImageTk.PhotoImage(image2)
+            button["image"]=photo2
+            button["width"]=65
+            button["height"]=75
+        if i==6 and j==3:
+            image3=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo3=ImageTk.PhotoImage(image3)
+            button["image"]=photo3
+            button["width"]=65
+            button["height"]=75
+        if i==6 and j==4:
+            image4=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo4=ImageTk.PhotoImage(image4)
+            button["image"]=photo4
+            button["width"]=65
+            button["height"]=75
+        if i==6 and j==5:
+            image5=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo5=ImageTk.PhotoImage(image5)
+            button["image"]=photo5
+            button["width"]=65
+            button["height"]=75
+        if i==6 and j==6:
+            image6=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo6=ImageTk.PhotoImage(image6)
+            button["image"]=photo6
+            button["width"]=65
+            button["height"]=75
+        if i==6 and j==7:
+            image7=Image.open("C:/Users/isalph2/OneDrive/Documents/GitHub/ELIT_chess/chess_game/gui/pawn.png")
+            photo7=ImageTk.PhotoImage(image7)
+            button["image"]=photo7
+            button["width"]=65
+            button["height"]=75
+        
 
 
 root.mainloop()

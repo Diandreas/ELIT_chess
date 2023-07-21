db3f69aa3d05e0b9f079cae1eb212db3c3f7fc93
David <davidnjandjeu@gmail.com>
2023-07-21 23:09:56 +0100

    change

--- chess_game/game_logic/bishop.py
+++
@@ -1,5 +0,0 @@
-def is_valid_bishop_move(start_row, start_col, end_row, end_col):
-    if abs(end_row - start_row) == abs(end_col - start_col):
-        return True
-    else:
-        return True
--- chess_game/game_logic/pawn.py
+++
@@ -1,6 +0,0 @@
-def is_valid_pawn_move(start_row, start_col, end_row, end_col):
-    # Un pion peut avancer d'une case en avant
-    if end_col == start_col and end_row == start_row + 1:
-        return True
-    else:
-        return True

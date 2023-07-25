a6562f8b69ec4eeaf0d3ecd7ceb8e459ed49150c
David <davidnjandjeu@gmail.com>
2023-07-25 08:38:14 +0100

    tout bouge  deja sans une logique

--- chess/engine.py
+++ chess/engine.py
@@ -1,20 +1,53 @@
-
-
-
-class GameSate():
-    def _init_(self):
+class GameState:
+    def __init__(self):
         self.board = [
-            ['/white/rook', '/white/knight', '/white/bishop', '/white/queen', '/white/king', '/white/bishop', '/white/knight', '/white/rook'],
-            ['/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn'],
+            ['/white/rook', '/white/knight', '/white/bishop', '/white/queen', '/white/king', '/white/bishop',
+             '/white/knight', '/white/rook'],
+            ['/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn', '/white/pawn',
+             '/white/pawn'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['--', '--', '--', '--', '--', '--', '--', '--'],
-            ['/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn'],
-            ['/black/rook', '/black/knight', '/black/bishop', '/black/king', '/black/queen', '/black/bishop', '/black/knight', '/black/rook'],
+            ['/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn', '/black/pawn',
+             '/black/pawn'],
+            ['/black/rook', '/black/knight', '/black/bishop', '/black/king', '/black/queen', '/black/bishop',
+             '/black/knight', '/black/rook'],
 
         ]
         self.whiteToMove = True
-        self.moveLog= []
+        self.moveLog = []
 
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove #changer de joueur
+
+
+
+
+
+
+
+
+class Move():
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
 
--- chess/main.py
+++ chess/main.py
@@ -1,38 +1,91 @@
 import pygame as p
-from Chess import engine
+import engine
 
 ...
+
 WIDTH = HEIGHT = 512
 DIMENSION = 8
-SQ_SIZE = HEIGHT //DIMENSION
+SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 15
-IMAGES= {}
+IMAGES = {}
 ...
+
+
 def loadImages():
-    pieces = ['pawn','king','knight','queen','rook','bishop']
-    for piece in pieces :
-        IMAGES[piece]=p.image.loade()
+    pieces = ['/white/pawn', '/white/king', '/white/knight', '/white/queen', '/white/rook', '/white/bishop',
+              '/black/pawn', '/black/king', '/black/knight', '/black/queen', '/black/rook', '/black/bishop']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("assets\model\piece_model_1" + piece + ".png"),
+                                          (SQ_SIZE, SQ_SIZE))
 
 
+...
 
 
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = engine.GameState()
+    loadImages()
+    running = True
+    sqSelected = ()  # selectioner les sq seelectionner
+    playerClicks = []  # surveiller les click
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sqSelected == (row, col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = engine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = []
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+    # print(gs.board)
 
 
-def drawGameState (screen, gs):
-    drawboard(screan) #dessiner les carre sur leechiquier
+...
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)  # dessiner les carre sur leechiquier
     # ajjouter les suggesion de mouvement
-    drawPieces(screan,gs.board)
+    drawPiece(screen, gs.board)
 
 
 def drawBoard(screen):
-    colors = [p.Color("white"] , p.Color("gray")]
-    ...
+    colors = [p.Color("white"), p.Color("light green")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r + c) % 2]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
-def drawPiece(screen,board)
-    pass
+
+def drawPiece(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
-
-if __name=="main":
+if __name__ == "main":
     main()
 
+main()
